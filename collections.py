03ad13fa3aff15ac39a9cbd1320c2b37dea6c189
12mchowdhury18 <125027028+12mchowdhury18@users.noreply.github.com>
2023-02-10 10:47:19 +0000

    Add files via upload

---
+++ collections.py
@@ -0,0 +1,32 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+myFruitList[2] = "chicken"
+
+print(myFruitList)
+
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+
+myFavoriteFruitDictionary = {
+  "Leo" : "apple",
+  "Eugene" : "banana",
+  "KAKA" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+
+print(myFavoriteFruitDictionary["Leo"])
